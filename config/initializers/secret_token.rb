4b0b03318db751bd62d828a3de4a4231b6e30ec1
Billy Spottswood <billy.spottswood@gmail.com>
2013-10-07 21:13:39 -0400

    message

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Omr4::Application.config.secret_key_base = '04b490f4f9cf6ff1436538c93b4dba24206b2265237802d63fae7e1a2c6d8c1f9f91c9e5ba87d54e7d9f668e31e68b6d74579fadbf100e77dc6f89c7263c1087'
