c8ab1602b934206fdd9c76e998a00dfb22bcc390
Billy Spottswood <billy.spottswood@gmail.com>
2013-10-19 02:53:13 -0400

    Added statute text

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
          
   has_many :pins
+
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
 end
